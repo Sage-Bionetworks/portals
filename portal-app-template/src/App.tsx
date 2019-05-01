ecfd9d86c6960e498c503f759fa2608741cc2868
Michael Lee <michael.leem42@gmail.com>
2019-05-01 09:37:19 -0700

    restructured project

---
+++ portal-app-template/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import './App.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header" />
+      hello
+    </div>
+  )
+}
+
+export default App
