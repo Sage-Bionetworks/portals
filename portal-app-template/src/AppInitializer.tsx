f2ac0aaaaec15df8b857802f7441f60b7155034a
Michael Lee <michael.leem42@gmail.com>
2019-05-08 12:09:36 -0700

    simplified run.sh

--- portal-app-template/src/AppInitializer.tsx
+++ portal-app-template/src/AppInitializer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { withRouter } from 'react-router-dom'
 import docTitleConfig from './example-configuration/docTitleConfig'
+import { SynapseClient } from 'synapse-react-client';
 
 export type AppInitializerProps = {
   location: any
@@ -13,10 +14,17 @@ class AppInitializer extends React.Component<AppInitializerProps, {}> {
   }
 
   componentDidMount() {
-    // TODO: Extract token
     if (document.title !== docTitleConfig.name) {
       document.title = docTitleConfig.name
     }
+    SynapseClient.getSessionTokenFromCookie().then(
+      (sessionToken) => {
+        // TODO: Pass up token throughout application
+        this.setState({ token: sessionToken })
+      }
+    ).catch((_err) => {
+      console.log('no token from cookie could be fetched ', _err)
+    })
   }
 
   componentDidUpdate(prevProps: any) {
--- run.sh
+++ run.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 # USAGE
 #   Sync current with staging:
-#     ./run.sh --push-staging --all
 #     ./run.sh --push-staging --[portal-name]
 #
 #   Sync production with production:
-#     ./run.sh --WARNING-push-production --all
 #     ./run.sh --WARNING-push-production --[portal-name]
 
 cd portal-app-template
 yarn # install dependencies
 cd ../
-# ls -d */ looks at only the directories below
-for dir in `ls -d portal-configurations/*/`
-do
-  # check if dir has changed since last build 
-  git diff HEAD....
-  if [[ $2 && $2 = "--all" || $?]]; then
-    # if that directory has been updated in this commit
-    cp -r $dir portal-app-template/example-configuration
-    cd portal-app-template
-    yarn build
-    if [ "$1" = "--WARNING-push-production" ]; then
-      echo "pushing local to production"
-      chmod +x ./example-configuration/scripts/exportS3ProductionBucketName
-      ./example-configuration/scripts/exportS3ProductionBucketName
-      aws s3 sync --delete --cache-control max-age=3000 $S3_PRODUCTION_BUCK_LOCATION
-    elif [ "$1" = "--push-staging" ]; then
-      echo "pushing local to staging"
-      chmod +x ./example-configuration/scripts/exportS3StagingBucketName
-      ./example-configuration/scripts/exportS3StagingBucketName
-      aws s3 sync --delete --cache-control max-age=0 ./build $S3_STAGING_BUCKET_LOCATION
-    cd ../
-    fi
-  else
-    echo $dir "didn't pass"
+cp -r portal-configuration/$2 portal-app-template/example-configuration
+yarn build portal-app-template/
+if [ "$1" = "--WARNING-push-production" ]; then
+  echo "pushing local to production"
+  chmod +x ./example-configuration/scripts/exportS3ProductionBucketName
+  ./example-configuration/scripts/exportS3ProductionBucketName
+  # check name
+  if [ -z "$S3_PRODUCTION_BUCK_LOCATION" ]; then
+    echo 'Error: exportS3ProductionBucketName must export bash variable: S3_PRODUCTION_BUCK_LOCATION'
+    exit 1
   fi
+  aws s3 sync --delete --cache-control max-age=3000 $S3_PRODUCTION_BUCK_LOCATION
+elif [ "$1" = "--push-staging" ]; then
+  echo "pushing local to staging"
+  chmod +x ./example-configuration/scripts/exportS3StagingBucketName
+  ./example-configuration/scripts/exportS3StagingBucketName
+  if [ -z "$S3_STAGING_BUCKET_LOCATION" ]; then
+    echo 'Error: exportS3StagingBucketName must export bash variable: S3_STAGING_BUCKET_LOCATION'
+    exit 1
+  fi
+  aws s3 sync --delete --cache-control max-age=0 ./build $S3_STAGING_BUCKET_LOCATION
+fi
 done
