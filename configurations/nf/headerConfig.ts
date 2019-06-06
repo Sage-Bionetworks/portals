da3c78b2aeeaf1b38e0c38d322f1231f1aaa94d9
Michael Lee <michael.leem42@gmail.com>
2019-06-06 13:24:08 -0700

    update nf text, update header font size

--- configurations/nf/headerConfig.ts
+++ configurations/nf/headerConfig.ts
@@ -2,7 +2,7 @@ import { HomePageHeaderConfig } from '../types/portal-config'
 
 const homePageHeader: HomePageHeaderConfig = {
   title: 'A home for Neurofibromatosis research resources',
-  summary: 'The NF Data Portal is a conduit for ongoing and published research activity related to Neurofibromatosis and Schwannomatosis. It contains data, analysis tools, and publications with the goal of accelerating the development of new treatments for NF.',
+  summary:  'A home for Neurofibromatosis research resources. The NF Data Portal was created to help openly explore and share NF datasets, analysis tools, resources, and publications related to neurofibromatosis and schwannomatosis. Anyone can join the NF Open Science Initiative (NF-OSI) to contribute!',
 }
 
 export default homePageHeader
