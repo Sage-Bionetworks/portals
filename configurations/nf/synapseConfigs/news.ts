9493d5a2058346a635d1858d6ba7f87d2703ca1f
jay-hodgson <jay.hodgson@sagebionetworks.org>
2019-08-19 11:49:05 -0700

    and update to point to prod WordPress site

--- configurations/nf/synapseConfigs/news.ts
+++ configurations/nf/synapseConfigs/news.ts
@@ -6,20 +6,20 @@ const news: SynapseConfig = {
       menuConfig: [
         {
           feedName:'Data Releases',
-          feedUrl:'https://portalnewsstg.wpengine.com/?feed=rss2&cat=4',
+          feedUrl:'https://portalnews.wpengine.com/?feed=rss2&cat=4',
           defaultItemsToShow:3,
         },
         {
           feedName:'Updates',
-          feedUrl:'https://portalnewsstg.wpengine.com/?feed=rss2&cat=3',
+          feedUrl:'https://portalnews.wpengine.com/?feed=rss2&cat=3',
           defaultItemsToShow:3,
           twitterFeedUrl:'https://twitter.com/AMPADPortal?ref_src=twsrc%5Etfw'
         },
         {
           feedName:'Newsletter',
-          feedUrl:'https://portalnewsstg.wpengine.com/?feed=rss2&cat=5',
+          feedUrl:'https://portalnews.wpengine.com/?feed=rss2&cat=5',
           defaultItemsToShow:3,
-          mailChimpSignupText:'Sign up to receive the AD Knowledge Portal\'s quarterly newsletter',
+          mailChimpSignupText:'Subscribe to the AMP-AD quarterly newsletter. Use the unsubscribe link in those e-mails to opt out at any time.  We will not share your email with any third parties or use it for any other purposes',
           mailChimpUrl:'https://sagebase.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=96b614587a'
         },
       ]
