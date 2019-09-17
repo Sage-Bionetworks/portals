bf4c999ee0fa392aa4f9da56b9e3999d6972dbda
Michael Lee <michael.leem42@gmail.com>
2019-09-17 08:43:22 -0700

    added help sub pages

--- configurations/drug-nomiation-tool/routesConfig.ts
+++ configurations/drug-nomiation-tool/routesConfig.ts
@@ -23,12 +23,38 @@ const routes: GenericRoute [] = [
     isNested: false,
     synapseConfigArray: []
   },
-  // TODO: Put in drug nomination tool below
   {
     name: 'Help',
     to: '/Help',
-    isNested: false,
-    synapseConfigArray: []
+    isNested: true,
+    routes: [
+      {
+        name: 'How It Works',
+        to: '/Help/How It Works',
+        isNested: false,
+        synapseConfigArray: [{
+          name: 'Markdown',
+          title: 'How It Works',
+          props: {
+            ownerId: 'syn20717442',
+            wikiId: '595391'
+          }
+        }]
+      },
+      {
+        name: 'Data Requirements',
+        to: '/Help/Data Requirements',
+        isNested: false,
+        synapseConfigArray: [{
+          name: 'Markdown',
+          title: 'Data Requirements',
+          props: {
+            ownerId: 'syn20717442',
+            wikiId: '595544'
+          }
+        }]
+      },
+    ]
   },
 ]
 
