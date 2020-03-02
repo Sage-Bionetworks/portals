79a22fa7e86ea2ea2b7c7ce68a4dc2e727400925
Michael Lee <michael.leem42@gmail.com>
2020-03-02 15:55:10 -0800

    added consistent org pages to all nf orgs

--- src/configurations/nf/synapseConfigs/organizationConfigs/cdmrp.ts
+++ src/configurations/nf/synapseConfigs/organizationConfigs/cdmrp.ts
@@ -1,5 +1,13 @@
 import { SynapseConstants } from 'synapse-react-client'
 import { BaseRoute } from 'types/portal-config'
+import { buttonColors } from '../commonProps'
+import { generateOrgConfig } from './generateConfig'
+import { studiesEntityId } from '../studies'
+import { datasetsEntityId } from '../datasets'
+import { filesEntityId } from '../files'
+import { publicationsEntityId } from '../publications'
+
+const org = 'CDMRP'
 
 export const cdmrp: BaseRoute = {
   name: 'CDMRP-NFRP',
@@ -16,6 +24,38 @@ export const cdmrp: BaseRoute = {
         entityId: 'syn16858699',
       },
       title: 'CDMRP Neurofibromatosis Research Program',
+    },
+    {
+      name: 'StatefulButtonControlWrapper',
+      props: {
+        ...buttonColors,
+        configs: [
+          {
+            name: 'Studies',
+            sql: generateOrgConfig(org, 'Studies', true),
+            entityId: studiesEntityId,
+            synapseConfigArray: generateOrgConfig(org, 'Studies', false),
+          },
+          {
+            name: 'Datasets',
+            sql: generateOrgConfig(org, 'Dataset', true),
+            entityId: datasetsEntityId,
+            synapseConfigArray: generateOrgConfig(org, 'Dataset', false),
+          },
+          {
+            name: 'Files',
+            sql: generateOrgConfig(org, 'Files', true),
+            entityId: filesEntityId,
+            synapseConfigArray: generateOrgConfig(org, 'Files', false),
+          },
+          {
+            name: 'Publications',
+            sql: generateOrgConfig(org, 'Publications', true),
+            entityId: publicationsEntityId,
+            synapseConfigArray: generateOrgConfig(org, 'Publications', false),
+          },
+        ],
+      },
     },
   ],
 }
