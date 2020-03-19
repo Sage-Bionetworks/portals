05af1b0b125d38646cba7817dd875a2ff95d719f
Michael Lee <michael.leem42@gmail.com>
2020-03-19 13:50:38 -0700

    added theme and icons to bsmn

--- src/configurations/bsmn/headerConfig.ts
+++ src/configurations/bsmn/headerConfig.ts
@@ -1,8 +1,8 @@
 import { HomePageHeaderConfig } from 'types/portal-config'
 
 const homePageHeader: HomePageHeaderConfig = {
-  summary: `Discover and download data and analysis from BSMN.`,
-  title: 'Welcome to the BSMN Knowledge Portal',
+  summary: `Access large-scale datasets and tools from a multi-site effort funded by the National Institute of Mental Health to investigate the genetic architecture of neuropsychiatric disease.`,
+  title: 'Welcome to the Brain Somatic Mosaicism Portal',
 }
 
 export default homePageHeader
