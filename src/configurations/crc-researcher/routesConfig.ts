75e3fb7594e4286acb1f8e940f762e8729ae2e0e
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-08-24 09:55:48 -0700

    update column alias names for plot labels

--- src/configurations/crc-researcher/routesConfig.ts
+++ src/configurations/crc-researcher/routesConfig.ts
@@ -32,7 +32,7 @@ const routes: GenericRoute[] = [
         isOutsideContainer: false,
         props: {
           widgetparamsMapped:{
-              query:'SELECT "date", CONTACT as "New accounts created", survey_1 as "Survey 1", survey_2 as "Survey 2", survey_3 as "Survey 3", survey_4 as "Survey 4" FROM syn22314856',
+              query:'SELECT "date", CONTACT as "New accounts created", survey_1 as "Completed Survey 1", survey_2 as "Completed Survey 2", survey_3 as "Completed Survey 3", survey_4 as "Completed Survey 4" FROM syn22314856',
               title:'New Participants Per Survey Per Day',
               xtitle:'Date',
               ytitle: 'Count',
