86e18b0e549e9a15efb17496aa0cd13fb956634d
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-08-25 15:40:36 -0700

    modify query for upset plot data from syn20821313

--- src/configurations/psychencode/routesConfig.ts
+++ src/configurations/psychencode/routesConfig.ts
@@ -30,7 +30,7 @@ const routes: GenericRoute[] = [
         centerTitle: true,
         props: {
           sql:
-            'SELECT distinct individualID, assay FROM syn20821313 where individualID is not null',
+            'SELECT unnest(individualID), assay FROM syn20821313 WHERE individualID is not null GROUP BY assay, unnest(individualID)',
           rgbIndex: 0,
           maxBarCount: 20,
           setName: 'Individuals (#) per Assay',
