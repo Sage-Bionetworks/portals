f4637dad2b57c57404b71a21ead74235cec8b0dc
Michael Lee <michael.leem42@gmail.com>
2020-09-08 16:22:35 -0700

    fix tests

--- src/RouteResolver.tsx
+++ src/RouteResolver.tsx
@@ -24,12 +24,8 @@ export const getRouteFromParams = (pathname: string) => {
   if (split[split.length - 1].match(/index\.html?/gim)) {
     // remove index.html
     split.pop()
-    if (split.length == 1) {
-      // need to have at least 2 items
-      split.push('')
-    }
   }
-  let route = routesConfig.find((el) => split[1] === el.to)!
+  let route = routesConfig.find((el) => split[0] === el.to)!
   // search the route configs for the pathname
   for (let i = 1; i < split.length; i += 1) {
     if (!route) {
