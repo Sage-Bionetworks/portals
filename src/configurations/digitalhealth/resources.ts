390a41e5c1ea7912c78e6c8dfed2166ed859501a
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-09-25 09:44:51 -0700

    PORTALS-1676: add table/view versions

--- src/configurations/digitalhealth/resources.ts
+++ src/configurations/digitalhealth/resources.ts
@@ -1,8 +1,8 @@
-export const dataSql = `SELECT id, studyOrProject, numberParticipants,reportedOutcome,dataCollectionMethod,deviceType,devicePlatform,deviceLocation,sensorType,diagnosis,digitalAssessmentCategory,digitalAssessmentDetails,dataType,dataSubtype,dataDescriptionLocation, dataAccessInstructions FROM syn21994970 where dhPortalIndex = 'TRUE'`
+export const dataSql = `SELECT id, studyOrProject, numberParticipants,reportedOutcome,dataCollectionMethod,deviceType,devicePlatform,deviceLocation,sensorType,diagnosis,digitalAssessmentCategory,digitalAssessmentDetails,dataType,dataSubtype,dataDescriptionLocation, dataAccessInstructions FROM syn21994970.2 where dhPortalIndex = 'TRUE'`
 export const publicationSql =
-  'SELECT * FROM syn22017695 ORDER BY "Year" DESC, "Title" ASC'
+  'SELECT * FROM syn22017695.1 ORDER BY "Year" DESC, "Title" ASC'
 export const projectsSql =
-  "SELECT * FROM syn21994974 WHERE  dhPortalIndex = 'TRUE' and isDHProject = 'TRUE' ORDER BY 'study'"
+  "SELECT * FROM syn21994974.4 WHERE  dhPortalIndex = 'TRUE' and isDHProject = 'TRUE' ORDER BY 'study'"
 export const studySql =
-  "SELECT * FROM syn21994974 WHERE ((isDHProject IS NULL) OR (isDHProject <> 'TRUE')) AND (dhPortalIndex = 'TRUE') ORDER BY 'study'"
-export const toolsSql = 'SELECT * FROM syn22014091 ORDER BY "softwareName"'
+  "SELECT * FROM syn21994974.4 WHERE ((isDHProject IS NULL) OR (isDHProject <> 'TRUE')) AND (dhPortalIndex = 'TRUE') ORDER BY 'study'"
+export const toolsSql = 'SELECT * FROM syn22014091.1 ORDER BY "softwareName"'
