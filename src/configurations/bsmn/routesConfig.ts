b2a0810755df517e0047b7d36ba83974f7b3a8af
jay-hodgson <jay.hodgson@sagebionetworks.org>
2020-10-13 15:32:17 -0700

    PORTALS-1700: use new people home page component

--- src/configurations/bsmn/routesConfig.ts
+++ src/configurations/bsmn/routesConfig.ts
@@ -12,6 +12,7 @@ import publications from './synapseConfigs/publications'
 import tools from './synapseConfigs/tools'
 import people from './synapseConfigs/people'
 import { studiesSql, projectsSql } from './resources'
+import {SynapseConstants} from "synapse-react-client";
 
 const routes: GenericRoute[] = [
   {
@@ -33,6 +34,32 @@ const routes: GenericRoute[] = [
           entityId: 'syn23021143',
         },
       },
+      {
+        name: 'UserCardListGroups',
+        title: 'Our Grants and People',
+        centerTitle: true,
+        outsideContainerClassName: 'home-spacer home-bg-dark',
+        props: {
+          sql: 'SELECT * FROM syn21781196 WHERE isFeatured=\'true\'',
+          columnName: 'Project Title',
+          facetValues: [
+            'Somatic Mosaicism in the brain of Tourette syndrome',
+            'Somatic Mosaicism in Schizophrenia and Control Brains',
+            '1/3-Schizophrenia Genetics and Brain Somatic Mosaicism',
+            '2/3-Schizophrenia Genetics and Brain Somatic Mosaicism',
+            'Role of brain somatic mosaicism in autism, schizophrenia, and bipolar disorder',
+            'Mosaicism in focal cortical dysplasias spectrum seen in neuropsychiatric disease',
+            '1/2-Somatic mosaicism and autism spectrum disorder',
+            '2/2-Somatic mosaicism and autism spectrum disorder',
+            '3/3-Schizophrenia Genetics and Brain Somatic Mosaicism',
+          ],
+          size: SynapseConstants.MEDIUM_USER_CARD,
+          useQueryResultUserData: false,
+          summaryLinkText:'EXPLORE ALL PEOPLE',
+          summaryLink: '/Explore/People',
+          count: 6
+        }
+      }
     ],
   },
   {
