76bc29e674c793c39a9ba22507a2c52cf921bf20
kelshmo <kelsey.montgomery@sagebase.org>
2020-10-16 14:37:22 -0700

    remove version from view as it breaks download list

--- src/configurations/psychencode/resources.ts
+++ src/configurations/psychencode/resources.ts
@@ -1,7 +1,7 @@
 // Portal owners can change the versions of the resources by modifying the
 // version of the entity used in the sql below
 export const studiesSql = 'SELECT * FROM syn21783965.2'
-export const dataSql = 'SELECT * FROM syn20821313.2'
+export const dataSql = 'SELECT * FROM syn20821313'
 export const metadataSql = 'SELECT id, metadataType, dataType, assay FROM syn20821313 WHERE "dataSubtype" = \'metadata\''
 export const peopleSql = 'SELECT * FROM syn22096112.2'
 export const grantSql = 'SELECT * FROM syn22096130.3'
