1259b20d18eec6eba5f8c9991227ff175acb3184
Jay Hodgson <jhodgson@Jays-MacBook-Air.local>
2020-11-03 09:56:21 -0800

    PORTALS-1747: lock down home page news feed to posts that are tagged with what's-new

--- src/configurations/adknowledgeportal/routesConfig.ts
+++ src/configurations/adknowledgeportal/routesConfig.ts
@@ -125,7 +125,10 @@ const routes: GenericRoute[] = [
           itemsToShow:3,
           allowCategories: ['Data Release', 'News', 'Webinar','rosMAP'],
           mailChimpListName: 'AMP-AD quarterly newsletter',
-          mailChimpUrl:'https://sagebase.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=96b614587a'
+          mailChimpUrl:'https://sagebase.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=96b614587a',
+          lockedFacet: {
+            value: 'what\'s-new'
+          }
         }
       },
     ],
