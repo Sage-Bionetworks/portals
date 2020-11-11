d156861b1be6d34f97cc508fa3731dd78d74808d
kelshmo <kelsey.montgomery@sagebase.org>
2020-11-11 13:19:13 -0500

    test view version with download list and increment version to fix data error

--- src/configurations/psychencode/resources.ts
+++ src/configurations/psychencode/resources.ts
@@ -1,8 +1,8 @@
 // Portal owners can change the versions of the resources by modifying the
 // version of the entity used in the sql below
 export const studiesSql = 'SELECT * FROM syn21783965.2'
-export const dataSql = 'SELECT * FROM syn20821313'
+export const dataSql = 'SELECT * FROM syn20821313.3'
 export const metadataSql = 'SELECT id, metadataType, dataType, assay FROM syn20821313 WHERE "dataSubtype" = \'metadata\''
 export const peopleSql = 'SELECT * FROM syn22096112.2'
-export const grantSql = 'SELECT * FROM syn22096130.3'
+export const grantSql = 'SELECT * FROM syn22096130.4'
 export const publicationSql = 'SELECT * FROM syn22095937.3 order by authors asc'
