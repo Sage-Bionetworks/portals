4a7c77f7e26a3fff2ff61ea16e808d96303df506
kelshmo <kelsey.montgomery@sagebase.org>
2020-11-13 16:05:14 -0500

    Remove study missing public data

--- src/configurations/psychencode/resources.ts
+++ src/configurations/psychencode/resources.ts
@@ -1,6 +1,6 @@
 // Portal owners can change the versions of the resources by modifying the
 // version of the entity used in the sql below
-export const studiesSql = 'SELECT * FROM syn21783965.2'
+export const studiesSql = 'SELECT * FROM syn21783965.3'
 export const dataSql = 'SELECT * FROM syn20821313.3'
 export const metadataSql = 'SELECT id, metadataType, dataType, assay FROM syn20821313 WHERE "dataSubtype" = \'metadata\''
 export const peopleSql = 'SELECT * FROM syn22096112.2'
