1c883def2cf6cec4e7bf910b5a4b890113eb21fd
Jay Hodgson <jay.hodgson@sagebionetworks.org>
2020-11-23 11:02:01 -0800

    PORTALS-1758: convert nf explore tools from QueryWrapperMenu to QueryWrapperPlotNav

--- src/configurations/nf/synapseConfigs/tools.ts
+++ src/configurations/nf/synapseConfigs/tools.ts
@@ -42,52 +42,6 @@ export const toolsCardConfiguration: CardConfiguration = {
     link: 'link',
   },
 }
-
-const searchConfiguration = {
-  searchable: [
-    {
-      columnName: 'name',
-      hintText: 'Browser',
-    },
-    {
-      columnName: 'summary',
-      hintText: 'prediction',
-    },
-    {
-      columnName: 'studyName',
-      hintText: 'Nerve Sheath',
-    },
-    {
-      columnName: 'fundingAgency',
-      hintText: 'NTAP',
-    },
-    {
-      columnName: 'contact',
-      hintText: 'Serra',
-    },
-    {
-      columnName: 'type',
-      hintText: 'Computational',
-    },
-    {
-      columnName: 'subtype',
-      hintText: 'animal model',
-    },
-    {
-      columnName: 'diseaseFocus',
-      hintText: 'Neurofibromatosis 2',
-    },
-    {
-      columnName: 'manifestation',
-      hintText: 'MPNST',
-    },
-  ],
-}
-
-const computationalSql =
-  "SELECT * FROM syn16859448 WHERE type = 'computational'"
-const experimentalSql = "SELECT * FROM syn16859448 WHERE type = 'experimental'"
-const clinicalSql = "SELECT * FROM syn16859448 WHERE type = 'clinical'"
 const rgbIndex = 6
 export const toolsEntityId = 'syn16859448'
 
@@ -105,105 +59,28 @@ const tools: HomeExploreConfig = {
     },
   },
   explorePageSynapseObject: {
-    name: 'QueryWrapperMenu',
+    name: 'QueryWrapperPlotNav',
     props: {
       rgbIndex,
-      facetAliases,
-      name: 'Tools',
-      globalQueryCountSql: 'SELECT * FROM syn16859448',
+      sql: 'SELECT * FROM syn16859448',
       shouldDeepLink: true,
-      accordionConfig: [
-        {
-          name: 'Computational',
-          cardConfiguration: toolsCardConfiguration,
-          searchConfiguration,
-          menuConfig: [
-            {
-              sql: computationalSql,
-              facet: 'studyName',
-            },
-            {
-              sql: computationalSql,
-              facet: 'fundingAgency',
-            },
-            {
-              sql: computationalSql,
-              facet: 'subtype',
-            },
-            {
-              sql: computationalSql,
-              facet: 'diseaseFocus',
-            },
-            {
-              sql: computationalSql,
-              facet: 'manifestation',
-            },
-            {
-              sql: computationalSql,
-            },
-          ],
-        },
-        {
-          name: 'Experimental',
-          cardConfiguration: toolsCardConfiguration,
-          searchConfiguration,
-          menuConfig: [
-            {
-              sql: experimentalSql,
-              facet: 'studyName',
-            },
-            {
-              sql: experimentalSql,
-              facet: 'fundingAgency',
-            },
-            {
-              sql: experimentalSql,
-              facet: 'subtype',
-            },
-            {
-              sql: experimentalSql,
-              facet: 'diseaseFocus',
-            },
-            {
-              sql: experimentalSql,
-              facet: 'manifestation',
-            },
-            {
-              sql: experimentalSql,
-            },
-          ],
-        },
-        {
-          name: 'Clinical',
-          cardConfiguration: toolsCardConfiguration,
-          searchConfiguration,
-          menuConfig: [
-            {
-              sql: clinicalSql,
-              facet: 'studyName',
-            },
-            {
-              sql: clinicalSql,
-              facet: 'fundingAgency',
-            },
-            {
-              sql: clinicalSql,
-              facet: 'subtype',
-            },
-            {
-              sql: clinicalSql,
-              facet: 'diseaseFocus',
-            },
-            {
-              sql: clinicalSql,
-              facet: 'manifestation',
-            },
-            {
-              sql: clinicalSql,
-            },
-          ],
-        },
-      ],
+      name: 'Tools',
+      cardConfiguration: toolsCardConfiguration,
+      facetAliases,
+      searchConfiguration: {
+        searchable: [
+          'name',
+          'summary',
+          'studyName',
+          'fundingAgency',
+          'contact',
+          'type',
+          'subtype',
+          'diseaseFocus',
+          'manifestation',
+        ],
+      },
+
     },
   },
 }
