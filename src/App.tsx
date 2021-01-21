4b19e07ef22b908854f067190ea4b866cd437126
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-01-21 15:35:06 -0500

    Remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import * as React from 'react'
 import './App.css'
-import './Markdown.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import { Footer } from './Footer'
 import AppInitializer from './AppInitializer'
