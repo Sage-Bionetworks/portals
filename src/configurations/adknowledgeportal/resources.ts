65bf0260a668089508994e0283cafbf65edb7b50
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2021-05-20 17:57:58 -0700

    new data file view version

--- src/configurations/adknowledgeportal/resources.ts
+++ src/configurations/adknowledgeportal/resources.ts
@@ -1,7 +1,7 @@
 export const computationalSql = 'Select * from syn20337467'
 export const toolSql = 'select * from syn22219805'
-export const dataSql = 'SELECT * FROM syn11346063.12'
-export const dataOnStudiesPageSql = "SELECT id, metadataType, dataType, assay FROM syn11346063.12 WHERE `resourceType` = 'metadata'"
+export const dataSql = 'SELECT * FROM syn11346063.13'
+export const dataOnStudiesPageSql = "SELECT id, metadataType, dataType, assay FROM syn11346063.13 WHERE `resourceType` = 'metadata'"
 export const peopleSql = 'SELECT * FROM syn13897207'
 export const projectsSql = 'SELECT * FROM syn17024229 ORDER BY isFeatured DESC'
 export const publicationsSql = 'SELECT * FROM syn20448807'
