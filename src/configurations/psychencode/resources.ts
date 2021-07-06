6b6dbc8449cf3b4f762e3684c49910f7b2205752
kelshmo <kelsey.montgomery@sagebase.org>
2021-07-05 21:30:46 -0500

    increment table view versions for latest release

--- src/configurations/psychencode/resources.ts
+++ src/configurations/psychencode/resources.ts
@@ -1,11 +1,11 @@
 // Portal owners can change the versions of the resources by modifying the
 // version of the entity used in the sql below
 
-const dataSynId = 'syn20821313.6'
-export const studiesSql = 'SELECT * FROM syn21783965.5'
+const dataSynId = 'syn20821313.7'
+export const studiesSql = 'SELECT * FROM syn21783965.6'
 export const dataSql = `SELECT * FROM ${dataSynId}`
 export const metadataSql = `SELECT id, metadataType, dataType, assay FROM ${dataSynId} WHERE "dataSubtype" = \'metadata\'`
 export const peopleSql = 'SELECT * FROM syn22096112.3'
 export const grantSql = 'SELECT * FROM syn22096130.4'
-export const publicationSql = 'SELECT * FROM syn22095937.4 order by authors asc'
+export const publicationSql = 'SELECT * FROM syn22095937.5 order by authors asc'
 export const upsetplotSql = `SELECT unnest(individualID), assay FROM ${dataSynId} WHERE individualID is not null GROUP BY assay, unnest(individualID)`
