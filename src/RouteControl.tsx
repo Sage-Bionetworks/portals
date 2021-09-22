251e6baba61a9fba34d99448b12a5fbbcba71180
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-09-22 12:20:39 -0400

    PORTALS-2015

--- src/RouteControl.tsx
+++ src/RouteControl.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import Dropdown from 'react-bootstrap/esm/Dropdown'
+import { useShowDesktop } from 'utils'
 
 export type NamedRoute = {
   name: string
@@ -7,26 +9,85 @@ export type NamedRoute = {
 export type RouteControlProps = {
   handleChanges: (text: string, index: number) => void
   isSelected: (name: string) => boolean
-  customRoutes: string[]  
+  customRoutes: string[]
 }
+
+const MAX_ROUTES_TO_SHOW = 7
 
 export const RouteControl: React.FunctionComponent<RouteControlProps> = ({
   handleChanges,
   isSelected,
-  customRoutes,  
+  customRoutes,
 }) => {
   const setActiveClass = (isSelected: boolean) =>
     isSelected ? 'isSelected' : ''
+
+  const indexOfActiveRoute = customRoutes.findIndex((name, index) =>
+    isSelected(name),
+  )
+
+  const isMobileView = !useShowDesktop()
+
+  /**
+   * In the Desktop (non-mobile) view, we limit the number of routes to show
+   */
+  let customRoutesToShow: string[]
+  if (isMobileView || customRoutes.length <= MAX_ROUTES_TO_SHOW) {
+    customRoutesToShow = customRoutes
+  } else {
+    customRoutesToShow = customRoutes.filter((route, index) => {
+      if (indexOfActiveRoute < MAX_ROUTES_TO_SHOW - 1) {
+        return index < MAX_ROUTES_TO_SHOW - 1
+      } else {
+        return index < MAX_ROUTES_TO_SHOW - 2 || isSelected(route)
+      }
+    })
+  }
+  const customRoutesInDropdown = customRoutes.filter(
+    (route) => !customRoutesToShow.includes(route),
+  )
+
   return (
     <nav className="flex-display nav explore-nav">
-      {customRoutes.map((name, index) => {
+      {customRoutesToShow.map((name, index) => {
         const handleClick = () => handleChanges(name, index)
         return (
-          <button onClick={handleClick} key={name} className={`nav-button nav-button-container center-content ${setActiveClass(isSelected(name))}`}>
+          <button
+            onClick={handleClick}
+            key={name}
+            className={`nav-button nav-button-container center-content ${setActiveClass(
+              isSelected(name),
+            )}`}
+          >
             {name}
           </button>
         )
       })}
+      {customRoutesInDropdown.length > 0 && (
+        <Dropdown>
+          <Dropdown.Toggle
+            variant="light"
+            className="nav-button nav-button-container"
+          >
+            {' '}
+            More
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {customRoutesInDropdown.map((name, index) => {
+              const handleClick = () => handleChanges(name, index)
+              return (
+                <Dropdown.Item
+                  onClick={handleClick}
+                  key={name}
+                  className={`nav-button nav-button-container`}
+                >
+                  {name}
+                </Dropdown.Item>
+              )
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      )}
     </nav>
   )
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
+
 // This code scrolls an element into view, and accounts for the fixed top nav bar height.
-export const scrollToWithOffset = (el:HTMLElement) => {
+export const scrollToWithOffset = (el: HTMLElement) => {
   const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset
   const yOffset = -90
   window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' })
+}
+
+const MOBILE_VIEWPORT_MAX_WIDTH_PX = 768
+
+export function useShowDesktop(breakpoint?: number) {
+  let usedBreakpoint = breakpoint ?? MOBILE_VIEWPORT_MAX_WIDTH_PX
+  const [showDesktop, setShowDesktop] = useState(
+    window.innerWidth > usedBreakpoint,
+  )
+  useEffect(() => {
+    const listener = () => {
+      const updatedValue = window.innerWidth > usedBreakpoint
+      if (updatedValue !== showDesktop) {
+        setShowDesktop(updatedValue)
+      }
+    }
+    window.addEventListener('resize', listener)
+    return () => {
+      window.removeEventListener('resize', listener)
+    }
+  })
+
+  return showDesktop
 }
