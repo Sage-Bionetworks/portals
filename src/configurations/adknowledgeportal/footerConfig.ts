2bef86b255bed38cbbb8f09817e3eb685f6f0a4b
Jay Hodgson <jay.hodgson@sagebase.org>
2021-09-29 13:58:27 -0700

    PORTALS-2019: make footer config termsOfService optional.  If not set, default to Synapse ToU pdf

--- src/configurations/adknowledgeportal/footerConfig.ts
+++ src/configurations/adknowledgeportal/footerConfig.ts
@@ -1,7 +1,6 @@
 import { FooterConfig } from 'types/portal-config'
 
 const footer: FooterConfig = {
-  termsOfService: 'https://docs.synapse.org/articles/governance.html',
   about: '/About',
 }
 
--- src/configurations/psychencode/footerConfig.ts
+++ src/configurations/psychencode/footerConfig.ts
@@ -1,8 +1,6 @@
 import { FooterConfig } from 'types/portal-config'
 
 const footer: FooterConfig = {
-  termsOfService:
-    'https://s3.amazonaws.com/static.synapse.org/governance/SageBionetworksSynapseTermsandConditionsofUse.pdf?v=5',
   contactUs: 'mailto:info@psychencode.org',
 }
 
