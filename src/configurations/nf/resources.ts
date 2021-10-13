81767e836554f35ad5d518154f1f6742042fab2f
anngvu <32753274+anngvu@users.noreply.github.com>
2021-10-13 16:31:52 -0700

    Update to latest snapshot - v7

--- src/configurations/nf/resources.ts
+++ src/configurations/nf/resources.ts
@@ -4,8 +4,8 @@ export const studiesSql = 'SELECT * FROM syn16787123'
 export const initiativesSql = 'SELECT * FROM syn24189696'
 export const toolsSql = 'SELECT * FROM syn16859448'
 export const peopleSql = 'SELECT * FROM syn23564971'
-export const filesSql = `SELECT id AS "File ID", assay, dataType, diagnosis, tumorType,  species, individualID,  fileFormat, dataSubtype, nf1Genotype as "NF1 Genotype", nf2Genotype as "NF2 Genotype", studyName, fundingAgency, consortium, name AS "File Name", accessType, accessTeam  FROM syn16858331.6 WHERE resourceType = 'experimentalData'`
-export const metadataFilesSql = `SELECT id, dataType, assay, diagnosis, tumorType, species, individualID, fileFormat, dataSubtype, nf1Genotype, nf2Genotype, fundingAgency, consortium FROM syn16858331.6 where resourceType ='report'`
+export const filesSql = `SELECT id AS "File ID", assay, dataType, diagnosis, tumorType,  species, individualID,  fileFormat, dataSubtype, nf1Genotype as "NF1 Genotype", nf2Genotype as "NF2 Genotype", studyName, fundingAgency, consortium, name AS "File Name", accessType, accessTeam  FROM syn16858331.7 WHERE resourceType = 'experimentalData'`
+export const metadataFilesSql = `SELECT id, dataType, assay, diagnosis, tumorType, species, individualID, fileFormat, dataSubtype, nf1Genotype, nf2Genotype, fundingAgency, consortium FROM syn16858331.7 where resourceType ='report'`
 export const fundersSql = 'SELECT * FROM syn16858699'
 export const hackathonsSql = 'SELECT * FROM syn25585549'
 
