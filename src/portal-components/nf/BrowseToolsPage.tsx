6eca49230519d5e2d8227dd7d15e3e8171f60975
Jay Hodgson <jay.hodgson@sagebase.org>
2021-10-25 15:23:13 -0700

    hook up Browse Tools By Category buttons

--- src/portal-components/nf/BrowseToolsPage.tsx
+++ src/portal-components/nf/BrowseToolsPage.tsx
@@ -1,8 +1,10 @@
+import { toolsSql } from 'config/resources'
 import Layout from 'portal-components/Layout'
 import * as React from 'react'
 import { Button, Form } from 'react-bootstrap'
 import FeaturedToolsList from 'synapse-react-client/dist/containers/home_page/featured_tools/FeaturedToolsList'
 import IconSvg from 'synapse-react-client/dist/containers/IconSvg'
+import { Query } from 'synapse-react-client/dist/utils/synapseTypes'
 import { ReactComponent as AnimalModels } from './assets/animalmodels.svg'
 import { ReactComponent as Antibodies } from './assets/antibodies.svg'
 import { ReactComponent as Biobanks } from './assets/biobanks.svg'
@@ -11,29 +13,44 @@ import { ReactComponent as PlasmidsReagents } from './assets/plasmids-reagents.s
 
 const BrowseToolsPage = () => {
   const [searchText, setSearchText] = React.useState<string>('')
-  const gotoExploreTools = () => {
-    window.location.assign('/Explore/Tools')
+  const gotoExploreTools = (selectedResource?: string) => {
+    if (selectedResource) {
+      const query: Query = {
+        sql: toolsSql,
+        selectedFacets: [
+          {
+            concreteType: "org.sagebionetworks.repo.model.table.FacetColumnValuesRequest",
+            columnName: 'Resource Type',
+            facetValues: [selectedResource]
+          }
+        ]
+      }
+      window.location.assign(`/Explore/Tools?QueryWrapper0=${JSON.stringify(query)}`)
+    } else {
+      window.location.assign('/Explore/Tools')
+    }
   }
+
   return (
     <div className="browse-tools-page">
       <div className="SRC-portalCard SRC-portalCardHeader bootstrap-4-backport">
-        <div className="home-container-description header-text" style={{padding: 0, marginTop: 0, marginBottom: 0}}>
+        <div className="home-container-description header-text" style={{ padding: 0, marginTop: 0, marginBottom: 0 }}>
           <h2 className="center-title">NF Research Tools Database</h2>
           <p className="text-align-center">
-          The NF Research Tools Database aims to support the development of a robust research toolkit and lower the barrier of entry to neurofibromatosis (NF) research. The database includes NF-associated animal models, cell lines, antibodies, and genetic reagents and details on tool characteristics and sourcing, as well as observational and experimental data.
+            The NF Research Tools Database aims to support the development of a robust research toolkit and lower the barrier of entry to neurofibromatosis (NF) research. The database includes NF-associated animal models, cell lines, antibodies, and genetic reagents and details on tool characteristics and sourcing, as well as observational and experimental data.
           </p>
           <div className="center-content">
-            <Button className="pill-xl" variant="white" style={{marginTop: 50}}>SUBMIT A TOOL</Button>
+            <Button className="pill-xl" variant="white" style={{ marginTop: 50 }}>SUBMIT A TOOL</Button>
           </div>
         </div>
       </div>
       <div className="home-container-description">
-        <h2 className="title center-title" style={{marginTop: 50}}>What Tools Can We Help You Find?</h2>
+        <h2 className="title center-title" style={{ marginTop: 50 }}>What Tools Can We Help You Find?</h2>
         <div className="center-content">
           <div className="searchToolsRow">
             <div className="searchInputWithIcon">
               <IconSvg options={{ icon: 'searchOutlined' }} />
-              <Form.Control type="search" placeholder="" 
+              <Form.Control type="search" placeholder=""
                 value={searchText}
                 onChange={event => {
                   setSearchText(event.target.value)
@@ -45,7 +62,7 @@ const BrowseToolsPage = () => {
             </div>
           </div>
         </div>
-        <h2 className="title center-title" style={{marginTop: 50}}>Popular Searches</h2>
+        <h2 className="title center-title" style={{ marginTop: 50 }}>Popular Searches</h2>
         <div className="center-content">
           <div className="popularSearchesContainer">
             <a>Animal models for NF1 OPG</a>
@@ -63,29 +80,29 @@ const BrowseToolsPage = () => {
           Drill-down to explore specific types of NF research tools.
         </p>
         <div className="categories">
-          <button>
+          <button onClick={() => gotoExploreTools('Animal Model')}>
             <AnimalModels />
             <p>Animal Models</p>
           </button>
-          <button>
+          <button onClick={() => gotoExploreTools('Antibody')}>
             <Antibodies />
             <p>Antibodies</p>
           </button>
-          <button>
+          <button onClick={() => gotoExploreTools('Genetic Reagent')}>
             <PlasmidsReagents />
             <p>Plasmids/Reagents</p>
           </button>
-          <button>
+          <button onClick={() => gotoExploreTools('Cell Line')}>
             <CellLines />
             <p>Cell Lines</p>
           </button>
-          <button>
+          <button onClick={() => gotoExploreTools('Biobank')}>
             <Biobanks />
             <p>Biobanks</p>
           </button>
         </div>
-        <div className="center-content bootstrap-4-backport" style={{marginTop: 50}}>
-          <Button className="pill-xl" variant="primary" onClick={gotoExploreTools}>VIEW ALL TOOLS</Button>
+        <div className="center-content bootstrap-4-backport" style={{ marginTop: 50 }}>
+          <Button className="pill-xl" variant="primary" onClick={() => gotoExploreTools()}>VIEW ALL TOOLS</Button>
         </div>
       </Layout>
       <Layout outsideContainerClassName="home-spacer">
@@ -98,18 +115,18 @@ const BrowseToolsPage = () => {
           </p>
         </div>
         <div className="center-content">
-            <FeaturedToolsList
-              entityId={'syn26344826'}
-              idColumnName={'Resource_id'}
-              nameColumnName={'Resource Name'}
-              descriptionColumnName={'Description'}
-              typeColumnName={'Resource Type'}
-              dateColumnName={'Date Added'}
-              toolDetailPageURL={'/Explore/Tools/DetailsPage?Resource_id='}
-            />
-          </div>
-        <div className="center-content bootstrap-4-backport" style={{marginTop: 50}}>
-            <Button className="pill-xl" variant="primary" onClick={gotoExploreTools}>VIEW ALL TOOLS</Button>
+          <FeaturedToolsList
+            entityId={'syn26344826'}
+            idColumnName={'Resource_id'}
+            nameColumnName={'Resource Name'}
+            descriptionColumnName={'Description'}
+            typeColumnName={'Resource Type'}
+            dateColumnName={'Date Added'}
+            toolDetailPageURL={'/Explore/Tools/DetailsPage?Resource_id='}
+          />
+        </div>
+        <div className="center-content bootstrap-4-backport" style={{ marginTop: 50 }}>
+          <Button className="pill-xl" variant="primary" onClick={() => gotoExploreTools()}>VIEW ALL TOOLS</Button>
         </div>
       </Layout>
       <Layout outsideContainerClassName="home-spacer home-bg-dark">
@@ -118,10 +135,10 @@ const BrowseToolsPage = () => {
         </h2>
         <div className="center-content">
           <p className="description">
-            We are currently accepting submissions that describe any NF1-related mouse model, cell line, genetic reagent (e.g. plasmid, CRISPR), antibody, or biobank. If you have a tool that you would like to add to the Research Tools Database, please reach out to us and we will be in touch with how to submit your tool. 
+            We are currently accepting submissions that describe any NF1-related mouse model, cell line, genetic reagent (e.g. plasmid, CRISPR), antibody, or biobank. If you have a tool that you would like to add to the Research Tools Database, please reach out to us and we will be in touch with how to submit your tool.
           </p>
         </div>
-        <div className="center-content bootstrap-4-backport" style={{marginTop: 50}}>
+        <div className="center-content bootstrap-4-backport" style={{ marginTop: 50 }}>
           <Button className="pill-xl" variant="primary">SUBMIT A TOOL</Button>
         </div>
       </Layout>
