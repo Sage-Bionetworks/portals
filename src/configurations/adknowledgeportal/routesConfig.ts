856b2ab3657ac3f3b1c81ae6ba856f1dad968f0c
Jay Hodgson <jay.hodgson@sagebase.org>
2021-12-06 15:00:03 -0800

    PORTALS-2074: update to latest SRC and remove all allowable categories from the WP site

--- src/configurations/adknowledgeportal/routesConfig.ts
+++ src/configurations/adknowledgeportal/routesConfig.ts
@@ -189,7 +189,7 @@ const routes: GenericRoute[] = [
         props: {
           url: 'https://news.adknowledgeportal.org',
           itemsToShow: 3,
-          allowCategories: ['Data Release', 'News', 'Webinar', 'rosMAP'],
+          allowCategories: [],
           mailChimpListName: 'AMP-AD quarterly newsletter',
           mailChimpUrl:
             'https://sagebase.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=96b614587a',
