b514f02c930d95e8a1323e6b4db5c242d1d7dd20
verena <9377970+vpchung@users.noreply.github.com>
2022-04-18 15:16:33 -0700

    add "People" to Explore navbar

--- src/configurations/cancercomplexity/routeControlWrapperProps.ts
+++ src/configurations/cancercomplexity/routeControlWrapperProps.ts
@@ -3,7 +3,7 @@ import { RouteControlWrapperProps } from 'portal-components/RouteControlWrapper'
 const routeControlProps: RouteControlWrapperProps = {
   // this has to get overriden,
   synapseConfig: {} as SynapseConfig,
-  customRoutes: ['Grants', 'Publications', 'Datasets', 'Files', 'Tools'],
+  customRoutes: ['Grants', 'People', 'Publications', 'Datasets', 'Files', 'Tools'],
 }
 
 export default routeControlProps
