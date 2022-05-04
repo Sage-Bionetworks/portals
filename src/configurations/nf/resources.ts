2adba94969f4ec598e177e548bb726d0e73e0cfe
Robert Allaway <allaway@users.noreply.github.com>
2022-05-04 10:15:52 -0700

    Update filesSql

--- src/configurations/nf/resources.ts
+++ src/configurations/nf/resources.ts
@@ -4,7 +4,7 @@ export const studiesSql = 'SELECT * FROM syn16787123'
 export const initiativesSql = 'SELECT * FROM syn24189696'
 export const toolsSql = 'SELECT * FROM syn26438037'
 export const peopleSql = 'SELECT * FROM syn23564971'
-export const filesSql = `SELECT id AS "File ID", assay, dataType, diagnosis, tumorType,  species, individualID,  fileFormat, dataSubtype, nf1Genotype as "NF1 Genotype", nf2Genotype as "NF2 Genotype", studyName, fundingAgency, consortium, name AS "File Name", accessType, accessTeam, Resource_id  FROM syn16858331.28 WHERE resourceType = 'experimentalData'`
+export const filesSql = `SELECT name, assay, dataType, diagnosis, tumorType,  species, individualID,  fileFormat, dataSubtype, nf1Genotype as "NF1 Genotype", nf2Genotype as "NF2 Genotype", studyName, fundingAgency, consortium, name AS "File Name", accessType, accessTeam, Resource_id  FROM syn16858331.28 WHERE resourceType = 'experimentalData'`
 export const metadataFilesSql = `SELECT id, dataType, assay, diagnosis, tumorType, species, individualID, fileFormat, dataSubtype, nf1Genotype, nf2Genotype, fundingAgency, consortium FROM syn16858331.28 where resourceType ='report'`
 export const fundersSql = 'SELECT * FROM syn16858699'
 export const hackathonsSql = 'SELECT * FROM syn25585549'
