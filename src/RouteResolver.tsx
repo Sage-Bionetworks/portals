6ccd0ba3df0d6545309b8e31c7bf3a517052776c
Jay Hodgson <jay.hodgson@sagebase.org>
2022-05-26 16:16:13 -0700

    also clean up routing.  does not seem to be a need to do this extra history.push() in RouteResolver (it only results in an extra item in the history)

--- src/RouteResolver.tsx
+++ src/RouteResolver.tsx
@@ -2,7 +2,6 @@ import { isEmpty } from 'lodash-es'
 import * as React from 'react'
 import {
   RouteComponentProps,
-  useHistory,
   useLocation,
   withRouter,
 } from 'react-router'
@@ -142,7 +141,7 @@ const RouteResolver: React.FunctionComponent<RouteComponentProps> = () => {
   // Map this to route in configuration files
   const { pathname, search, hash } = useLocation()
   // get the route object and the typical path of the route
-  const [route, newPathname] = getRouteFromParams(pathname)
+  const [route] = getRouteFromParams(pathname)
   // If url has search params transform into key-value dictionary that can be passed into
   // the component which is rendered
   let searchParamsProps: any = undefined
@@ -163,22 +162,6 @@ const RouteResolver: React.FunctionComponent<RouteComponentProps> = () => {
     document.title = newTitle
   }
   const scrollToRef = React.useRef<HTMLElement>(null)
-
-  const history = useHistory()
-  React.useEffect(() => {
-    // We push the new pathname to history with one exception:
-    // If we landed on a route that redirects, then don't push the new pathname because it won't take into account the redirect
-    if (
-      (route.synapseConfigArray ?? []).filter(
-        (el) => (el.name === 'RedirectWithQuery' || el.name === 'Redirect'),
-      ).length === 0
-    ) {
-      //PORTALS-2057: if new path name contains a search query, do not include it more than once (will be represented in the search string)
-      const indexOfQuestionMark = newPathname.indexOf('?')
-      const pathNameWithoutSearch = indexOfQuestionMark > -1 ? newPathname.substring(0, indexOfQuestionMark) : newPathname
-      history.push({ pathname: pathNameWithoutSearch, search, hash })
-    }
-  }, [newPathname])
 
   // this delay is here to improve the location of the element, since it's position depends on the layout of other components on the page (that also need to load)
   setTimeout(() => {
