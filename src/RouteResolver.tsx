a23212ee4359c7f7095ee507796becfd64cce04a
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-07-12 10:15:42 -0400

    Change how the document title is updated

--- src/RouteResolver.tsx
+++ src/RouteResolver.tsx
@@ -1,10 +1,11 @@
 // import docTitleConfig from 'config/docTitleConfig'
 import * as React from 'react'
 import { Route, useLocation, useRouteMatch } from 'react-router-dom'
-import {SynapseComponent} from 'SynapseComponent'
+import { SynapseComponent } from 'SynapseComponent'
 import { SynapseConfig, GenericRoute } from 'types/portal-config'
 import { scrollToWithOffset } from 'utils'
-import routesConfig from './config/routesConfig'
+import docTitleConfig from 'config/docTitleConfig'
+import routesConfig from 'config/routesConfig'
 import Layout from './portal-components/Layout'
 import sharedRouteConfig from './shared-config/sharedRoutes'
 
@@ -90,16 +91,26 @@ function ComponentRenderer(props: { config: SynapseConfig }) {
   )
 }
 
-function RecursiveRouteRenderer(props: { route: GenericRoute }) {
-  const { route } = props
+function RecursiveRouteRenderer(props: {
+  route: GenericRoute
+  getPageNameFromParentRoute: () => string
+}) {
+  const { route, getPageNameFromParentRoute } = props
   const { url } = useRouteMatch()
 
-  // const pageName = route.displayName ?? route.path
-  // const newTitle: string = `${docTitleConfig.name} - ${pageName}`
-  // TODO:
-  // if (document.title !== newTitle) {
-  //   document.title = newTitle
-  // }
+  const pageName = route.displayName ?? route.path?.replaceAll('/', '')
+  const getPageName = () =>
+    pageName
+      ? `${docTitleConfig.name} - ${pageName}`
+      : getPageNameFromParentRoute()
+
+  // if there are children, don't update the title
+  if (!route.routes || route.routes.length === 0) {
+    const newTitle: string = getPageName()
+    if (document.title !== newTitle) {
+      document.title = newTitle
+    }
+  }
 
   return (
     <>
@@ -117,7 +128,10 @@ function RecursiveRouteRenderer(props: { route: GenericRoute }) {
               path={`${url}${url.endsWith('/') ? '' : '/'}${r.path}`}
               exact={r.exact}
             >
-              <RecursiveRouteRenderer route={r} />
+              <RecursiveRouteRenderer
+                route={r}
+                getPageNameFromParentRoute={getPageName}
+              />
             </Route>
           )
         })}
@@ -143,7 +157,12 @@ const RouteResolver = () => {
     })
   }
 
-  return <RecursiveRouteRenderer route={ROUTES} />
+  return (
+    <RecursiveRouteRenderer
+      route={ROUTES}
+      getPageNameFromParentRoute={() => docTitleConfig.name}
+    />
+  )
 }
 
 export default RouteResolver
