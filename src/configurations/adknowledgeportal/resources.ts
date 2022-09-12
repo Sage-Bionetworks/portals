6b15fa536fb4780b66081d85e22d810457db58e9
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-09-12 11:10:48 -0700

    increment AD file view for data release 19.8

--- src/configurations/adknowledgeportal/resources.ts
+++ src/configurations/adknowledgeportal/resources.ts
@@ -1,7 +1,7 @@
 export const computationalSql = 'Select * from syn20337467'
-export const dataSql = 'SELECT * FROM syn11346063.29'
+export const dataSql = 'SELECT * FROM syn11346063.30'
 export const dataOnStudiesPageSql =
-  "SELECT id, name, metadataType, dataType, assay FROM syn11346063.29 WHERE `resourceType` = 'metadata'"
+  "SELECT id, name, metadataType, dataType, assay FROM syn11346063.30 WHERE `resourceType` = 'metadata'"
 export const peopleSql = 'SELECT * FROM syn13897207'
 export const projectsSql = 'SELECT * FROM syn17024229 ORDER BY isFeatured DESC'
 export const publicationsSql = 'SELECT * FROM syn20448807'
