9b0012ceef25cc15b17475cc8798c3ab1fbeb027
Jay Hodgson <jay.hodgson@sagebase.org>
2022-09-14 13:29:36 -0700

    point footer About link to about page

--- src/configurations/arkportal/footerConfig.ts
+++ src/configurations/arkportal/footerConfig.ts
@@ -1,7 +1,7 @@
 import { FooterConfig } from 'types/portal-config'
 
 const footer: FooterConfig = {
-  about: 'https://help.arkportal.org/About.1994096680.html',
+  about: '/About',
 }
 
 export default footer
